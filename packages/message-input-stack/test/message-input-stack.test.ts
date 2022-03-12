9eef3a0118910c6970522cd3ca21e4309427081f
Oliver Ross <oliver.ross@hotmail.com>
2022-03-12 21:15:54 +0000

    explicitly test for fifo topic

--- packages/message-input-stack/test/message-input-stack.test.ts
+++ packages/message-input-stack/test/message-input-stack.test.ts
@@ -2,9 +2,24 @@ import * as cdk from 'aws-cdk-lib'
 import { Template } from 'aws-cdk-lib/assertions'
 import { MessageInputStack } from '../lib/message-input-stack'
 
-it('Stack matches snapshot', () => {
-  const app = new cdk.App()
-  const stack = new MessageInputStack(app, 'TestMessageInputStack')
-  const template = Template.fromStack(stack)
-  expect(template.toJSON()).toMatchSnapshot()
+describe('message input stack', () => {
+  let template: Template
+
+  beforeAll(() => {
+    const app = new cdk.App()
+    const stack = new MessageInputStack(app, 'TestMessageInputStack')
+    template = Template.fromStack(stack)
+  })
+
+  it('describes a send message topic with the correct properties', () => {
+    template.hasResourceProperties('AWS::SNS::Topic', {
+      ContentBasedDeduplication: true,
+      FifoTopic: true,
+      TopicName: 'send-message-topic.fifo'
+    })
+  })
+
+  it('matches the snapshot', () => {
+    expect(template.toJSON()).toMatchSnapshot()
+  })
 })
