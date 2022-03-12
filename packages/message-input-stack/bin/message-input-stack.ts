02029b556c04934bab46f6a4414e1f4aec19687e
Oliver Ross <oliver.ross@hotmail.com>
2022-03-12 11:34:01 +0000

    lint / test tooling setup

--- packages/message-input-stack/bin/message-input-stack.ts
+++ packages/message-input-stack/bin/message-input-stack.ts
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
-import "source-map-support/register";
-import * as cdk from "aws-cdk-lib";
-import { MessageInputStackStack } from "../lib/message-input-stack-stack";
+/* eslint-disable no-new */
+import 'source-map-support/register'
+import * as cdk from 'aws-cdk-lib'
+import { MessageInputStackStack } from '../lib/message-input-stack-stack'
 
-const app = new cdk.App();
-new MessageInputStackStack(app, "MessageInputStackStack", {
+const app = new cdk.App()
+new MessageInputStackStack(app, 'MessageInputStackStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -15,4 +16,4 @@ new MessageInputStackStack(app, "MessageInputStackStack", {
    * want to deploy the stack to. */
   // env: { account: '123456789012', region: 'us-east-1' },
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+})
--- packages/message-input-stack/test/message-input-stack.test.ts
+++ packages/message-input-stack/test/message-input-stack.test.ts
@@ -4,7 +4,7 @@
 
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/message-input-stack-stack.ts
-test("SQS Queue Created", () => {
+test('SQS Queue Created', () => {
   //   const app = new cdk.App();
   //     // WHEN
   //   const stack = new MessageInputStack.MessageInputStackStack(app, 'MyTestStack');
@@ -13,4 +13,4 @@ test("SQS Queue Created", () => {
   //   template.hasResourceProperties('AWS::SQS::Queue', {
   //     VisibilityTimeout: 300
   //   });
-});
+})
